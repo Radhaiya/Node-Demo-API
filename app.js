bc7a73292901acc726b2cfb59be3d839626c0936
Radhaiya <kulkarnir27999@gmsil.com>
2022-07-21 21:03:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express()
+
+console.log("ass")
+
+
+app.get('/', (req, res) => {
+    res.send("welcome")
+
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log("working")
+});
+    
